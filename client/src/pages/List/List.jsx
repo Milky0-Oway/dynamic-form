a81eeb3199af02a192527e644523fcbacdf7b0e9
Milky0-Oway <milka.korotkaya@gmail.com>
2024-10-11 17:50:40 +0300

    fix: made minor fixes

--- client/src/pages/List/List.jsx
+++ client/src/pages/List/List.jsx
@@ -4,7 +4,7 @@ import styles from './List.module.css';
 
 export const List = () => {
     const navigate = useNavigate();
-    
+
     const handleCreate = () => {
         navigate('/create');
     };
@@ -13,13 +13,13 @@ export const List = () => {
         <main className={styles.container}>
             <header className={styles.header}>
                 <h1>List of contacts</h1>
-                    <button
-                        className={styles.create}
-                        onClick={handleCreate}
-                        aria-label="Create New Contact"
-                    >
-                        Create New Contact
-                    </button>
+                <button
+                    className={styles.create}
+                    onClick={handleCreate}
+                    aria-label="Create New Contact"
+                >
+                    Create New Contact
+                </button>
             </header>
             <section>
                 <ContactList />
